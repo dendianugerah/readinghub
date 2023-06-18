8e83b8e96fe761ee17c32c73b24c4a39206ed899
intern.dendi <intern.dendi@privyid.tech>
2023-06-18 16:00:33 +0700

    feat(book-detail): add new page, detail book

--- components/template/book/detail/index.vue
+++ components/template/book/detail/index.vue
@@ -4,9 +4,30 @@
       {{ state.hero.headline }}
     </div>
     <div class="detail-book-container">
-      <div class="detail-book-1">1</div>
-      <div class="detail-book-1">2</div>
-      <div class="detail-book-1">3</div>
+      <div class="detail-book-container-img">
+        <img :src="state.hero.image" alt="" />
+      </div>
+      <div class="detail-book-container-content">
+        <h1>
+          {{ state.hero.title }}
+        </h1>
+        <span>
+          {{ state.hero.description }}
+        </span>
+      </div>
+      <div class="detail-book-container-form">
+        <table>
+          <tbody>
+            <tr v-for="(value, key) in state.book" :key="key">
+              <td class="py-2 px-4">{{ formatKey(key) }}</td>
+              <td class="py-2 px-4">: {{ value }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="center-button">
+      <router-link to="/mid" class="back-link">Kembali</router-link>
     </div>
   </div>
 </template>
@@ -17,39 +38,52 @@ import { reactive } from "~/.nuxt/imports";
 const state = reactive<{
   hero: {
     headline: string;
-  };
-  book: {
     title: string;
     description: string;
-    code: string;
-    category: string;
-    publisher: string;
-    author: string;
-    amount: string;
-    ISBN: string;
-    publicationYear: string;
-    RAK: string;
     image: string;
+  };
+  book: {
+    Kode: string;
+    Kategori: string;
+    Penerbit: string;
+    Pengarang: string;
+    Jumlah: string;
+    ISBN: string;
+    Tahun: string;
+    RAK: string;
   };
 }>({
   hero: {
     headline: "Detail Buku",
-  },
-  book: {
     title: "Mencuri Raden Dendi",
     description:
       "Novel ini membawa kita melintasi perjalanan yang menggugah hati, mulai dari hubungannya dengan keluarga, guru seninya yang inspiratif, hingga perjalanannya ke Eropa yang mengubah cara pandangnya tentang seni. Kita juga diajak menyaksikan pertemuannya dengan para seniman terkenal, perjuangannya mengatasi konflik internal, dan kontribusinya yang luar biasa dalam memperjuangkan hak-hak seniman di masa kolonial.",
-    code: "AB82973",
-    category: "Sains Fiksi",
-    publisher: "Amikom",
-    author: "Dendi Anugerah",
-    amount: "34",
-    ISBN: "45716215719",
-    publicationYear: "2021",
-    RAK: "P - SF - 3 - 1",
     image: "/static/img/donation/home/hero.png",
   },
+  book: {
+    Kode: "AB82973",
+    Kategori: "Sains Fiksi",
+    Penerbit: "Amikom",
+    Pengarang: "Dendi Anugerah",
+    Jumlah: "34",
+    ISBN: "45716215719",
+    Tahun: "2021",
+    RAK: "P - SF - 3 - 1",
+  },
 });
+
+function formatKey(key: string): string {
+  const formattedKey = key.replace(/ /g, "&nbsp;");
+  if (key === "Kode") {
+    return `${formattedKey} Buku`;
+  }
+
+  if (key === "Tahun") {
+    return `${formattedKey} Terbit`;
+  }
+
+  return formattedKey;
+}
 </script>
 
 <style lang="postcss" scoped>
@@ -57,11 +91,42 @@ const state = reactive<{
   @apply max-w-7xl mx-auto;
 
   &-title {
-    @apply text-center font-ptserif font-bold text-[40px];
+    @apply text-center font-ptserif font-bold text-[40px] pt-[40px] pb-[60px];
   }
 
   &-container {
-    @apply grid grid-cols-3 gap-4;
+    @apply grid grid-cols-3 gap-[33px];
+
+    &-content {
+      & > h1 {
+        @apply font-ptserif font-bold text-[40px] pb-[24px];
+      }
+
+      & > span {
+        @apply font-robotoslab text-[20px] text-[#272726];
+      }
+    }
+
+    &-form {
+      @apply font-robotoslab text-[20px];
+
+      & > button {
+        @apply mt-[24px] rounded-[32px] border-white border-[2px] w-[220px] h-[50px];
+        @apply font-robotoslab text-white font-bold;
+      }
+    }
+  }
+}
+
+.center-button {
+  @apply flex justify-center mt-[24px] pb-[80px];
+
+  & > .back-link {
+    @apply items-center;
+    @apply mt-[24px] rounded-[32px] border-black border-[2px] w-[220px] h-[50px];
+    @apply font-robotoslab text-black font-bold;
+    @apply flex justify-center items-center;
+    @apply no-underline;
   }
 }
 </style>
